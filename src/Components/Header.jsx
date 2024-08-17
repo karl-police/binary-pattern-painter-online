e0076dacf1a0780ec468e01ec287a64c467eef75
karl-police <karl-police2001@bluewin.ch>
2024-08-17 22:59:38 +0200

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Header() {
     return (<>
         <header>
-            <h1 align="center">Test</h1>
+            <h1 style={{textAlign: "center"}}>Binary Pattern Editor</h1>
         </header>
     </>)
 }
