ac012c4c571f11589ea55eed16089c1c4096ca0c
karl-police <karl-police2001@bluewin.ch>
2024-08-17 23:38:53 +0200

    Update Main_Page.jsx

--- src/pages/Main_Page.jsx
+++ src/pages/Main_Page.jsx
@@ -14,7 +14,7 @@ function Main_Page() {
             <div>
                 <h2>Links</h2>
                 <ul>
-                    <li>e</li>
+                    <li><Link to="/binary-pattern-editor">binary-pattern-editor</Link></li>
                 </ul>
             </div>
         </div>
