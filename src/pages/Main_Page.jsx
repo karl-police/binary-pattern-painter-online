0bf5638e05e0e4b2961cf268888a864e2075ded9
karl-police <karl-police2001@bluewin.ch>
2024-08-17 22:24:21 +0200

    Update Main_Page.jsx

--- src/pages/Main_Page.jsx
+++ src/pages/Main_Page.jsx
@@ -5,9 +5,19 @@ import Header from "../Components/Header";
 function Main_Page() {
     return (<>
         <div>
-            <ul>
-                <li><a href="./binary-pattern-editor">binary-pattern-editor</a></li>
-            </ul>
+            <div>
+                <h2>About</h2>
+                <p>
+                    This Website provides some tools to create data in shape of binary matrices and export them as
+                    JSON Array tables and or other.
+                </p>
+            </div>
+            <div>
+                <h2>Links</h2>
+                <ul>
+                    <li><a href="./binary-pattern-editor">binary-pattern-editor</a></li>
+                </ul>
+            </div>
         </div>
    </>)
 }
