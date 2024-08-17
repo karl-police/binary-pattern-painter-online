d7e75b65cbbab588ea0c96cf34d6fe0ef69637eb
karl-police <karl-police2001@bluewin.ch>
2024-08-17 21:53:11 +0200

    Create MainPage.jsx

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "../Components/Header";
+
+function MainPage() {
+    return (
+        <html>
+            <body>
+                <Header/>
+            </body>
+        </html>
+   )
+}
+
+export default MainPage;
