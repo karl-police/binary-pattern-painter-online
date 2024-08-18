07bb7d94c6fbce1b30b27732c0daf155728d8574
karl-police <karl-police2001@bluewin.ch>
2024-08-19 00:07:29 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,4 @@
-// @ts-nocheck
+import React from "react"
 
 // https://nextjs.org/docs/pages/building-your-application/routing/custom-document
 import { Html, Head, Main, NextScript } from "next/document"
