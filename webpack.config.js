502ce2abf7ea8b42a6f4eb300be0f3a603aafac4
karl-police <karl-police2001@bluewin.ch>
2024-08-18 16:40:07 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
-var path = require("path")
-var HWP = require("html-webpack-plugin")
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: path.join(__dirname, "./src/index.js"),
     output: {
-      filename: "build.js",
-      path: path.join(__dirname, "/dist")},
-      module:{
-          rules:[{
-              test: /\.js$/,
-              exclude: /node_modules/,
-              loader: "babel-loader"
-          }]
-      },
-      plugins:[
-          new HWP({template: path.join(__dirname, "./src/index.html")})
-      ]
-  }
+    filename: "build.js",
+    path: path.join(__dirname, "/dist")},
+    module:{
+        rules:[{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
+    },
+    plugins:[
+        new HtmlWebPackPlugin({
+            template: path.join(__dirname, "./src/index.html")
+        })
+    ]
+}
