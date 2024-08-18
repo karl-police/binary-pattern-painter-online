e3ffdd23d39fd96823fe76f9c742f52b7251d171
karl-police <karl-police2001@bluewin.ch>
2024-08-18 15:19:22 +0200

    Update BinaryPatternEditorPage.jsx

--- src/pages/BinaryPatternEditorPage.jsx
+++ src/pages/BinaryPatternEditorPage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-import test from "../js/test";
 
 function BinaryPatternEditorPage() {
     return (<>
+        <Helmet>
+            <script src="./js/test.js"></script>
+        </Helmet>
+        
         <div>
             test
         </div>
