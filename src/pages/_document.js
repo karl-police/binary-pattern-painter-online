16402ec4305bc996b03fb85be4a1b6817dbe4607
karl-police <karl-police2001@bluewin.ch>
2024-08-19 00:05:08 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,3 +1,6 @@
+// @ts-nocheck
+
+// https://nextjs.org/docs/pages/building-your-application/routing/custom-document
 import { Html, Head, Main, NextScript } from "next/document"
  
 export default function Document() {
