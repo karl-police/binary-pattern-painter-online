9afef2175fa7549f9068a4a650f35c2cfa106fa5
karl-police <karl-police2001@bluewin.ch>
2024-08-18 21:12:07 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,32 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+//import ReactDOM from "react-dom/client";
 
 import {Helmet} from "react-helmet";
 
 // CSS
 import '../css/main.css';
 
-
 import Header from "../Components/Header";
 
-//import reportWebVitals from './reportWebVitals';
+//const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return(<>
+    <Helmet
+      htmlAttributes={{
+        lang: "en"
+      }}
+      title="Binary Pattern Editor Online"
+    />
 
-root.render(<>
-  <Helmet
-    htmlAttributes={{
-      lang: "en"
-    }}
-    title="Binary Pattern Editor Online"
-  />
-
-  <Header></Header>
+    <Header></Header>
+  </>)
+}
 
 
-</>);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+export default App;
