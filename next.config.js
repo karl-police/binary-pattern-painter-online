2835d85044b9f8d7f602508cbceba38f9aa52207
karl-police <karl-police2001@bluewin.ch>
2024-08-18 17:33:31 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,7 +10,15 @@ const nextConfig = {
      */
     basePath: "/binary-pattern-painter-online",
 
-    assetPrefix: "./"
+    /**
+     * Disable server-based image optimization. Next.js does not support
+     * dynamic features with static exports.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+     */
+    images: {
+        unoptimized: true,
+    },
 };
 
 module.exports = nextConfig;
