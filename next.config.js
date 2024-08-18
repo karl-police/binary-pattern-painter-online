119c2bd6a832354639a6ec2f7d2ed5188a10cee7
karl-police <karl-police2001@bluewin.ch>
2024-08-18 17:30:57 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
     // enables static exports
     output: "export",
+
+    /**
+     * Set base path. This is the slug of your GitHub repository.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+     */
+    basePath: "/binary-pattern-painter-online",
+
     assetPrefix: "./"
 };
 
