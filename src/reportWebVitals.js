05cb786428dd9f8cdf986d05bed382acb37db0a3
karl-police <karl-police2001@bluewin.ch>
2024-08-18 21:42:52 +0200

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
