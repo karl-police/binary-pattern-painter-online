a338c387de5c77a957e6c06aec0c38a4c56e32f7
karl-police <karl-police2001@bluewin.ch>
2024-08-18 19:16:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 
 // Had to install this manually into Microsoft/TypeScript
-import {Route, BrowserRouter, Routes} from "react-router-dom";
+//import {Route, BrowserRouter, Routes} from "react-router-dom";
 import {Helmet} from "react-helmet";
 
 // CSS
@@ -32,18 +32,19 @@ root.render(<>
   <Header></Header>
 
   {/* process.env.PUBLIC_URL seems to be /binary-pattern-painter-online */}
-  <BrowserRouter basename={`${"/binary-pattern-painter-online"}`}>
-    <Routes>
-      {/* Main page, for gh-pages "/" needed to go away */}
-      <Route path="/" element={<Main_Page/>} />
-      <Route path="/index" element={<Main_Page/>} />
-      <Route path="/main" element={<Main_Page/>} />
-
-      {/* Rest of the paths */}
-      <Route path="/binary-pattern-editor" element={<BinaryPatternEditorPage/>}></Route>
-    </Routes>
-  </BrowserRouter>
+  
 </>);
+//<BrowserRouter basename={`${"/binary-pattern-painter-online"}`}>
+//<Routes>
+//{/* Main page, for gh-pages "/" needed to go away */}
+//<Route path="/" element={<Main_Page/>} />
+//<Route path="/index" element={<Main_Page/>} />
+//<Route path="/main" element={<Main_Page/>} />
+//
+//{/* Rest of the paths */}
+//<Route path="/binary-pattern-editor" element={<BinaryPatternEditorPage/>}></Route>
+//</Routes>
+//</BrowserRouter>
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
