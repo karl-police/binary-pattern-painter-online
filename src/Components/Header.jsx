b0dfbac47ecc8a2ca53c2621ea198810b2836662
karl-police <karl-police2001@bluewin.ch>
2024-08-18 03:41:52 +0200

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import PUBLIC_URL from "../modules/public_url.js";
 function Header() {
     return (<>
         <header>
-            <h1 style={{textAlign: "center"}}><a href={PUBLIC_URL} className="no-link-style">Binary Pattern Editor</a></h1>
+            <h1 style={{textAlign: "center"}}><a href={PUBLIC_URL.url} className="no-link-style">Binary Pattern Editor</a></h1>
         </header>
     </>)
 }
